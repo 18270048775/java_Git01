0ec9f2a67d0e1ca12431216c403ce812231bcbd8
mikasa <ErenMikasa@qq.com>
2023-01-06 18:32:08 +0800

    first commit

---
+++ src/main/java/org/gitDemo.java
@@ -0,0 +1,14 @@
+package org;
+
+/**
+ * Author Eren and Mikasa
+ * 道阻且长，行则将至
+ * Date 2023/1/6
+ * <p>
+ * Describe
+ */
+public class gitDemo {
+    public static void main(String[] args) {
+        System.out.println("hello git");
+    }
+}
